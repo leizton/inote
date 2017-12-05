fcc6f56947078d59ace2086c833f57210e7a108f
leizton <leizton@126.com>
2017-12-05 08:52:12 +0800

    git

--- detail/git.sh
+++ detail/git.sh
@@ -1,10 +1,13 @@
+# github验证用户权限
+ssh -T git@github.com
+
 # sshkey
-$ ssh-keygen -t rsa -C 'xxx@yyy.com'
+ssh-keygen -t rsa -C 'xxx@yyy.com'
 
 # base
-> git保存快照, svn保存文件的变化, 所有在切换分支时git速度比svn快
-> git用文件内容通过SHA-1 hash生成提交版本号, svn使用递增编号作版本号
-> git的3种文件状态: modified, staged, commited
+git保存快照, svn保存文件的变化, 所有在切换分支时git速度比svn快
+git用文件内容通过SHA-1 hash生成提交版本号, svn使用递增编号作版本号
+git的3种文件状态: modified, staged, commited
   已修改(工作区) --add--> 已暂存(暂存区) --commit--> 已提交(本地仓库)
 
 # 工作区 暂存区 版本库
@@ -18,83 +21,83 @@ git reset --hard	版本库 -> 暂存区 -> 工作区
 git branch -a
 
 # 删除远程分支
-$ git push origin --delete 分支名
+git push origin --delete 分支名
 
 # 拉取远程分支
 git pull包含 git fetch(把远程仓库的某个分支的HEAD拉取到本地) 和 git merge(把其他分支的HEAD合并到当前分支) 2个步骤
 格式：git pull 远程仓库名 远程分支[:同时要合并的另外一个本地分支]
 如果指定了--rebase，则不是执行git merge，而是执行git rebase
-$ git pull origin master:local  把远程分支master拉取下来后合并到本地的local分支，当前分支也合并了远程的master分支
-$ git pull origin master:master
+git pull origin master:local  把远程分支master拉取下来后合并到本地的local分支，当前分支也合并了远程的master分支
+git pull origin master:master
 
 # 查看远程仓库设置
-$ git remote -v
+git remote -v
 
 # clone
-$ git clone 地址 -b master  # 只clone下master分支
+git clone 地址 -b master  # 只clone下master分支
 
 # remote
-$ git remote add origin git@gitlab...(http://gitlab...)  # git@在push时无需输密码, http://需要
-$ git remote                # 查看远程仓库
-$ git remote remove origin  # 删除origin仓库
-$ git remote -v             # 查看远程仓库的地址
+git remote add origin git@gitlab...(http://gitlab...)  # git@在push时无需输密码, http://需要
+git remote                # 查看远程仓库
+git remote remove origin  # 删除origin仓库
+git remote -v             # 查看远程仓库的地址
 
 # log/reflog
 log只显示commit操作记录, reflog还会显示checkout切换分支的操作记录
-$ git log --graph --oneline
+git log --graph --oneline
 
 # diff
-$ git diff                       # 默认工作区和暂存区的差别
-$ git diff --cached(或--staged)  # 暂存区和本地仓库的差别
-$ git diff version1 version2     # 两个commit版本version1和version2的差别
-$ git diff master branch1        # 两个分支的差别
+git diff                       # 默认工作区和暂存区的差别
+git diff --cached(或--staged)  # 暂存区和本地仓库的差别
+git diff version1 version2     # 两个commit版本version1和version2的差别
+git diff master branch1        # 两个分支的差别
 
 # push
-$ git push -u origin branch1     # 绑定远程仓库的branch1分支, 以后直接push会推到branch1分支
+git push -u origin branch1     # 绑定远程仓库的branch1分支, 以后直接push会推到branch1分支
 
 # fetch/merge/pull
-$ git fetch                 # 拉取远程分支到本地
-$ git merge origin/branch1  # 把远程branch1与本地合并
-$ git pull                  # 合并上面两个命令
-$ git pull --rebase
-$ git merge --abort         # 取消conflict合并
+git fetch                 # 拉取远程分支到本地
+git merge origin/branch1  # 把远程branch1与本地合并
+git pull                  # 合并上面两个命令
+git pull --rebase
+git merge --abort         # 取消conflict合并
 
 # rebase和merge的区别: rebase线性合并, merge分叉合并
 合并前
 		  E---F  #本地master分支
 		 /
 	A---B---C---D  #origin/master，远程分支
-$ git merge master
+git merge master
 		  E-------F
 		 /         \
 	A---B---C---D---G  # master和origin/master，创建了一个新的commit(G)
-$ git rebase master
+git rebase master
 	A---B---C---D---E1---F1  # master和origin/master，E1 F1和E F的commit SHA序号不同
 
 # branch
-$ git branch dev  # 创建新分支(dev)
-$ git branch -a  # 显示本地和远程的所有分支
+git branch dev  # 创建新分支(dev)
+git branch -a  # 显示本地和远程的所有分支
 
 # checkout
-$ git checkout [HEAD] $file    # 已修改的工作区文件回退到当前提交
-$ git checkout 2588f237 $file  # 回退到版本2588f237
-$ git checkout -b dev          # 创建新分支，并切换新分支
+git checkout [HEAD] $file    # 已修改的工作区文件回退到当前提交
+git checkout 2588f237 $file  # 回退到版本2588f237
+git checkout -b dev          # 创建新分支，并切换新分支
 
 # reset
-$ git reset --soft  HEAD~  # 把HEAD指针指向上一个commit
-$ git reset --mixed HEAD~  # 把本地仓库的HEAD~_commit copy to staged区
-$ git reset --hard  HEAD~  # 把本地仓库的HEAD~_commit copy to工作区和staged区, 覆盖了当前修改
+git reset --soft  HEAD~  # 把HEAD指针指向上一个commit
+git reset --mixed HEAD~  # 把本地仓库的HEAD~_commit copy to staged区
+git reset --hard  HEAD~  # 把本地仓库的HEAD~_commit copy to工作区和staged区, 覆盖了当前修改
 
 # revert
-$ git revert HEAD~2  # 和git reset --hard HEAD~2的区别是: revert会保留HEAD HEAD~提交, reset --hard不会保留
+git revert HEAD~2  # 和git reset --hard HEAD~2的区别是: revert会保留HEAD HEAD~提交, reset --hard不会保留
 
 # cherry-pick
-$ git cherry-pick commitID  # 把其他分支的某个提交应用到当前分支, 仅应用这一个提交
+git cherry-pick commitID  # 把其他分支的某个提交应用到当前分支, 仅应用这一个提交
 
 # blame
-$ git blame Test.java  # 显示每行代码最后是谁在什么时间提交的
-$ find . -name "*.java" | xargs -l git blame --line-porcelain | sed -n 's/^author //p' | sort | uniq -c | sort -rn
-$ find . -name "*.java" | xargs -l git blame | grep -n "作者名"  # 查找某个人写的每行代码
+git blame Test.java  # 显示每行代码最后是谁在什么时间提交的
+find . -name "*.java" | xargs -l git blame --line-porcelain | sed -n 's/^author //p' | sort | uniq -c | sort -rn
+find . -name "*.java" | xargs -l git blame | grep -n "作者名"  # 查找某个人写的每行代码
 # 查找某个人写了哪些文件
 comm -12 <(find . -name "*.java" | sort) <(find . -name "*.java" | \
 	xargs -l git blame --line-porcelain | \
