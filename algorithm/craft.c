4d0723c4fccb9f29d9df7fc24b251b523459e2b0
leizton <leizton@126.com>
2018-02-26 01:39:39 +0800

    algo

--- algorithm/craft.c
+++ algorithm/craft.c
@@ -32,3 +32,71 @@ int alignToPowOf2(int n) {
     n |= (n>>16);
     return n+1;
 }
+
+// 统计数组的逆序对数目, 排序所需的最小交换次数
+// 分治思路, 归并排序
+template<typename T>
+int numOfInvertedPairs(T a[], int begin, int end) {
+    if (a == NULL || begin < 0 || end <= begin+1) {
+        return 0;
+    }
+
+    T* copy = new T[(end-begin) * 2];
+    std::memcpy(copy, a, (end-begin) * sizeof(T));
+    std::memcpy(copy+(end-begin), a, (end-begin) * sizeof(T));
+    int num = numOfInvertedPairsImpl(copy+(end-begin), copy, 0, end-begin);
+    delete[] copy;
+    return num;
+}
+template<typename T>
+int numOfInvertedPairsImpl(T src[], T copy[], int begin, int end) {
+    if (begin+1 == end) {
+        return 0;
+    }
+
+    // 把copy左右两边的排序结果分别放到src的左右边
+    // src的[begin,end)被打乱, 递归的上一层不受影响
+    // 例如, 当前层的下一层会分别打乱copy的[begin,mid)和[mid,end), 但不影响排序结果merge到copy
+    int mid = (begin + end) >> 1;
+    int num = numOfInvertedPairsImpl(copy, src, begin, mid);
+    num += numOfInvertedPairsImpl(copy, src, mid, end);
+
+    // 把src的左右merge到copy, 最终排序结果在copy上
+    int i = begin, j = mid, k = begin;
+    while (i < mid && j < end) {
+        if (src[i] <= src[j]) {
+            copy[k++] = src[i++];
+        } else {
+            copy[k++] = src[j++];
+            num += mid - i;
+        }
+    }
+    while (i < mid) {
+        copy[k++] = src[i++];
+    }
+    while (j < end) {
+        copy[k++] = src[j++];
+    }
+    return num;
+}
+
+// 打乱数组
+template<typename T>
+void shuffle(T a[], int begin, int end) {
+    if (a == NULL || begin < 0 || end <= begin) {
+        return;
+    }
+
+    static std::random_device rndDev;
+    static std::mt19937 rndGen(rndDev());
+    int j;
+
+    for (int i = begin + 1; i < end; ++i) {
+        // 只跟自己前面的元素交换
+        std::uniform_int_distribution<int> rnd(begin, i);
+        j = rnd(rndGen);
+        if (j != i) {
+            std::swap(a[i], a[j]);
+        }
+    }
+}
