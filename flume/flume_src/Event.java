d24a9d59eaad64b60246dab163c3fd7fe581ad92
leizton <leizton@126.com>
2018-03-30 09:36:39 +0800

    rm flume

--- flume/flume_src/Event.java
+++
@@ -1,6 +0,0 @@
-Event interface
-// 事件对象由 headers 和 body 组成
-> getHeaders():Map<String, String>
-> setHeaders(Map<String, String> headers)
-> getBody():byte[]
-> setBody(byte[] body)
--- flume/flume_src/LifecycleAware_LifecycleState.java
+++
@@ -1,8 +0,0 @@
-LifecycleState enum
-// 生命周期中的各个状态
-> IDLE, START, STOP, ERROR
-
-LifecycleAware interface
-> start()
-> stop()
-> getLifecycleState():LifecycleState
