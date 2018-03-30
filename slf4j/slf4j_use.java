89a98ccb5c1f157d058f7781f8aaf447f3c93ff4
leizton <leizton@126.com>
2018-03-30 09:41:06 +0800

    clear

--- slf4j/slf4j_use.java
+++
@@ -1,8 +0,0 @@
-class Foo {
-	private static final Logger LOGGER = LoggerFactory.getLogger(Foo.class);
-	static {
-		LOGGER.info("static_code_block: {}", "Foo.class");  // {}是占位符
-		LOGGER.error("", "{}, {}", "1", 2);
-		LOGGER.error(e.getMessage(), e);
-	}
-}
