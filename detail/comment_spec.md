5786c4f6c1dc7b816439d5fcfe733144c0805e73
leizton <leizton@126.com>
2018-04-17 16:54:55 +0800

    python simplehttpserver upload

--- detail/comment_spec.md
+++ detail/comment_spec.md
@@ -1,6 +1,7 @@
 # symbol
 //=          comment
 ~            comment
+//#-{flag}   注释flag, @ref #{flag}
 ()           variable's init value
 ::           attribute or field, replaced by '.'
 :            variable's type(no blank space)
---
+++ python/base/upload.py
@@ -0,0 +1,18 @@
+import sys
+import httplib
+
+
+def upload(host, port, srcfile):
+  body = open(srcfile, "rb").read()
+  conn = httplib.HTTPConnection(host, port)
+  conn.request("PUT", "/" + srcfile[srcfile.find('/')+1:], body)
+  resp = conn.getresponse()
+  print resp.status, resp.reason
+
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print('invalid argv')
+    exit(-1)
+  srcfile = sys.argv[1]
+  upload("127.0.0.1", 10000, srcfile)
