97d86f40bd21a6cb80fa375a0c8d3588fd7ba966
leizton <leizton@126.com>
2018-09-22 11:47:00 +0800

    bash_profile

--- detail/bash_profile.sh
+++ detail/bash_profile.sh
@@ -1,27 +1,26 @@
 export PS1='\[\033[1;31m\]\t \w]$(parse_git_branch)\[\033[0;m\] \n> '
 
 # docker
-function dock_start() {
-  docker start "$1"
+# docker ps -a
+# docker images
+# docker exec -it $container_name /bin/bash
+function docker_run() {
+  container_name="$1"
+  img_repo="$2"
+  docker run -dit --name $container_name $img_repo /bin/bash
 }
-function dock_rm() {
-  docker stop "$1" && docker rm "$1"
+function docker_mount() {
+  container_name="$1"
+  img_repo="$2"
+  src="$3"
+  dst="$4"
+  docker run -dit --name $container_name --mount type=bind,source=$src,target=$dst $img_repo /bin/bash
 }
-function dock_in() {
-  docker exec -it "$1" /bin/bash
-}
-function dock_run() {
-  docker run -dit --name "$1" "whiker/ubuntu16:$2" /bin/bash
-}
-function dock_mount() {
-  docker run -dit --name tmp --mount type=bind,source=$1,target=$2 whiker/ubuntu16:$3 /bin/bash
-}
-alias dock_ps='docker ps -a'
-alias dock_img='docker images'
 
 # docker util
-alias dock_practice="dock_mount '${HOME}/sky/practice' '/opt/practice' dev"
-alias dock_redis="dock_mount '${HOME}/code/store/redis' '/opt/redis' dev"
+function docker_practice() {
+  docker_mount 'practice' 'whiker/ubuntu18_dev:v1.0' '/Users/whiker/sky/practice' '/opt/practice'
+}
 
 # time
 function timest() {
@@ -52,16 +51,10 @@ alias gita="git add -A .";
 alias gitph="git push";
 alias gitpl="git pull";
 alias gitplr="git pull --rebase";
-alias gitb="git branch";
 alias gith="git reset --hard HEAD";
 alias gituser="git config user.name";
 alias gitemail="git config user.email";
-function gitconfname() {
-  git config user.name $@
-}
-function gitconfemail() {
-  git config user.email $@
-}
+alias gitconf="git config user.name fanyaoqun && git config user.email fanyaoqun@bytedance.com";
 function parse_git_branch() {
   b=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
   if [ -n "$b" ]; then
@@ -77,13 +70,16 @@ function gitl1() {
   git log -$num --format="%H  %cn  %s"
 }
 function gitch() {
-  git checkout $1
+  git checkout $@
 }
 function gitc() {
   git commit -m "$1"
 }
 function gitac() {
   git commit -a -m "$1"
+}
+function gitb() {
+  git branch $@
 }
 function gitbd_remote() {
   git push origin --delete $1
@@ -115,27 +111,27 @@ function avro_gen_java() {
 }
 
 # PATH
-PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}";
-export PATH;
+export ROCKSDB_HOME="/usr/local/Cellar/rocksdb/5.14.2";
+export PATH="$PATH:ROCKSDB_HOME/bin";
 
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home";
 export PATH="$PATH:$JAVA_HOME/bin";
 
-export GOROOT="${HOME}/bin/go-1.10.2";
-export GOPATH="${HOME}/bin/gopath";
+export GOROOT="/Users/whiker/bin/go-1.10.2";
+export GOPATH="/Users/whiker/bin/gopath";
 export GOBIN="$GOROOT/bin";
 export PATH="$PATH:$GOBIN";
 
-export M2_HOME="${HOME}/bin/apache-maven-3.3.9";
+export M2_HOME="/Users/whiker/bin/apache-maven-3.3.9";
 export PATH="$PATH:$M2_HOME/bin";
 
-export GRADLE_HOME="${HOME}/bin/gradle-3.2.1";
+export GRADLE_HOME="/Users/whiker/bin/gradle-3.2.1";
 export PATH="$PATH:$GRADLE_HOME/bin";
 
-export SCALA_HOME="${HOME}/bin/scala-2.12.1";
+export SCALA_HOME="/Users/whiker/bin/scala-2.12.1";
 export PATH="$PATH:$SCALA_HOME/bin";
 
-export CMAKE_HOME="${HOME}/bin/cmake-3.7.2";
+export CMAKE_HOME="/Users/whiker/bin/cmake-3.7.2";
 export PATH="$PATH:$CMAKE_HOME/bin";
 
 export SBT_HOME="/usr/local/Cellar/sbt/0.13.13";
@@ -144,10 +140,15 @@ export PATH="$PATH:$SBT_HOME/bin";
 export MYSQL_HOME="/usr/local/mysql";
 export PATH="$PATH:$MYSQL_HOME/bin";
 
-export PROTOC_HOME="${HOME}/bin/protobuf-2.5.0";
+export PROTOC_HOME="/Users/whiker/bin/protobuf-2.5.0";
 export PATH="$PATH:$PROTOC_HOME/src";
 
 alias python="/Library/Frameworks/Python.framework/Versions/3.5/bin/python3.5";
 alias pip="/Library/Frameworks/Python.framework/Versions/3.5/bin/pip3.5";
 
-export PKG_CONFIG_PATH="${HOME}/bin/libevent/lib/pkgconfig:$PKG_CONFIG_PATH";
+export PKG_CONFIG_PATH="/Users/whiker/bin/libevent/lib/pkgconfig:$PKG_CONFIG_PATH";
+
+export JULIA_HOME="/Applications/Julia-1.0.app/Contents/Resources/julia";
+export PATH="$JULIA_HOME/bin:$PATH"
+
+# user custom
