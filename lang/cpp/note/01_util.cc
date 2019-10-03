d123a04a47cee64b931ad64c6719530eff6ce60f
leizton <leizton@126.com>
2019-10-03 11:09:27 +0800

    cpp

--- lang/cpp/note/01_util.cc
+++ lang/cpp/note/01_util.cc
@@ -1,19 +1,30 @@
-# 模板
-- is_assignable<T, U>
-  判断是否可通过赋值运算符来转换
-```cpp
+# is_assignable<T, U>
+判断是否可通过赋值运算符来转换
 is_assignable<vector<int*>, vector<int*>>::value == true
 is_assignable<list<int*>, list<int*>>::value     == true
 is_assignable<vector<int*>, list<int*>>::value   == false
-//
+
 class Foo {
 public:
   Foo(int) {}
   Foo& operator=(const Foo&);
 };
-//
+
 is_assignable<Foo, int>::value == true
 is_assignable<Foo, Foo>::value == true
 if Foo(int)构造函数是explicit, is_assignable<Foo, int>::value==false
 if operator=()是private, is_assignable<Foo, Foo>::value==false
-```
+
+
+
+# std::tie
+获得std::tuple的各个值
+// ex
+double d;  int i;
+std::tie(d, i) = std::make_tuple(3.14, 1);
+
+// impl: tuple.cc
+template<typename... T>
+tuple<T&...> tie(T&... args) {
+  return tuple<T&...>(args...);
+}
--- lang/cpp/note/02_右值_move_forward.cc
+++ lang/cpp/note/02_右值_move_forward.cc
@@ -4,30 +4,42 @@
 右值引用和右值没有关系，仅指所引用的对象可以看成临时对象，其内容可被窃取过来
 右值引用一定是const &(常量引用)
 
+
+
 # 左右值重载
-```c++
 struct A {
   int& get() &  { return _v; }
   int  get() && { return _v; }  // 此重载的调用是右值，即*this是右值
 private:
   int _v;
 };
-```
+
+
 
 # std::move
 无条件转发成右值
-```c++
 // impl: move.h
 template<typename T>
 typename remove_reference<T>::type&& move(T&& t) {
   return static_cast<typename remove_reference<T>::type&&>(t);
 }
-```
+// ex: 减少拷贝
+string s = "abc";
+vector<string> v;
+v.push_back(s);     // 调用 push_back(T&&) 重载函数
+cout << s << endl;  // 空串
+//
+void test(int&& x) {
+  int&& y = std::move(x);  // 必须用move, 否则报左值不能绑定到右值, x被看成左值
+  int&& x1 = 3;
+  int&& y1 = std::move(x1);  // 也必须用move
+}
+
+
 
 # std::forward
 实现完美转发，左值转发成左值引用，右值转发成右值引用
 move是无条件都转发成右值引用，forward则保持原特性
-```c++
 // ex
 struct A {
   A(int& i)  { cout << "lvalue" << endl; }
@@ -42,18 +54,3 @@ unique_ptr<T> create(U&... u) {
 auto p1 = create<A>(1);  // rvalue
 int i = 0;
 auto p2 = create<A>(i);  // lvalue
-```
-
-# std::tie
-获得std::tuple的各个值
-```c++
-// ex
-double d;  int i;
-std::tie(d, i) = std::make_tuple(3.14, 1);
-
-// impl: tuple.cc
-template<typename... T>
-tuple<T&...> tie(T&... args) {
-  return tuple<T&...>(args...);
-}
-```
